917b7837d42c319b90e879cc45ba43e81e9aa4f1
Donray Williams <w.donray@yahoo.com>
2018-01-10 17:57:08 -0600

    Fixed

--- Class.py
+++ Class.py
@@ -1,32 +1,43 @@
+'''Parser'''
+
 class Parser(object):
     '''Parser'''
 
-    def __init__(self, string):
-        self.string_data = string
+    def __init__(self, expression):
+        self.string_data = expression
+        self.num_clauses = 0
+        self.storage = []
+        self.grammar = ['!', '+', '*', '(', ')']
 
     def get_variables(self):
-        storage = []
-        i = 0
-        for data in self.string_data:
-            if data is '(' or data is '+':
-                
-                if self.string_data[i+1] is '!':
-                    
-                    if (self.string_data[i+2],) not in storage:
-                        
-                        storage.append((self.string_data[i+2],))
-                        
-                elif (self.string_data[i+1],) not in storage:
-                    
-                    storage.append((self.string_data[i+1],))
-            i+=1
-        print storage
+        '''Get Variables'''
+        test = []
+        self.storage = self.string_data
+        for i in self.grammar:
+            self.storage = self.storage.replace(i, '')
+
+        for data in self.storage:
+            #Check for duplicates
+            if data not in test:
+                test.append(data)
+
+        self.storage = sorted(test, key = lambda x: x)
+        return self.storage
 
     def get_data_from_file(self, filename):
+        '''Get Data From File'''
         test_file = open(filename, 'ab+')
         self.string_data = test_file.read()
+
+    def get_amount_clause(self):
+        '''Get Clause Amount'''
+        self.num_clauses = 0
+        for data in self.string_data:
+            if data is '(':
+                self.num_clauses += 1
+        return 'Clause Amount: ' + str(self.num_clauses)
+
 
 if __name__ == '__main__':
     import Main as Main
     Main.main()
-
--- Main.py
+++ Main.py
@@ -2,12 +2,17 @@ import os
 import sys
 from Class import Parser
 
+
 def main():
-    test = Parser('(D+C)*(!O+B)')
-    test.get_variables()
+    '''Main'''
+    input1 = '(D+C)*(!O+B)'
+    test = Parser(input1)
+    print test.get_variables()
+    print test.get_amount_clause()
 
     test.get_data_from_file('data.txt')
-    test.get_variables()
+    print test.get_variables()
+    print test.get_amount_clause()
 
 
 if __name__ == "__main__":
